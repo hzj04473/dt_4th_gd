8772aa4b3af5e5947f2995657290a5d69f7943d5
Kim Jinkyoung <hzj04473@naver.com>
2024-11-29 12:00:46 +0900

    1129. javascript ex2 (실습2 - prompt 사용)

--- javascript/1129/js/ex2.js
+++ javascript/1129/js/ex2.js
@@ -1,5 +1,8 @@
-let mathScore = '77';
-let engScore = '88';
+let mathScore = prompt('수학 점수를 입력해 주세요. 기본점수 : 77점', 77);
+let engScore = prompt('영어 점수를 입력해 주세요. 기본점수 : 88', 88);
 
 avgScore = (Number(mathScore) + Number(engScore)) / 2;
+avgScore2 = (parseFloat(mathScore) + parseFloat(engScore)) / 2;
+
 console.log(`mathScore + engScore = avgScore (${avgScore})`);
+console.log(`mathScore + engScore = avgScore2 (${avgScore2})`);
