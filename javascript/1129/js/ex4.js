eecab8113e61f202e1dd742405b5d9dbd0aebd96
Kim Jinkyoung <hzj04473@naver.com>
2024-11-29 15:21:32 +0900

    1129. javascript ex4 (실습4 - function)

---
+++ javascript/1129/js/ex4.js
@@ -0,0 +1,10 @@
+console.log('function 실습2');
+
+let square = function (num1) {
+  let result = num1 * num1;
+  console.log(`${num1} * ${num1} = ${result}`);
+};
+
+square(4);
+square(11);
+square(5);
