bb65993e16d2b74367a48789cabb0287d5edc634
Kim Jinkyoung <hzj04473@naver.com>
2024-11-29 16:24:57 +0900

    1129. javascript ex5 (실습5 - conditional)

--- javascript/1129/js/ex5.js
+++ javascript/1129/js/ex5.js
@@ -2,17 +2,20 @@ console.log('conditional 실습');
 
 function isAge() {
   const age = prompt('나이를 입력해 주세요', 10);
-
-  if (age >= 20) {
-    return '성인';
-  } else if (age > 14 && age <= 17) {
-    return '고딩';
-  } else if (age > 8 && age <= 14) {
-    return '중딩';
-  } else if (age > 0 && age <= 8) {
-    return '초딩';
+  if (age > 100) {
+    return '장수👍';
   } else {
-    return '유아';
+    if (age >= 20) {
+      return '성인';
+    } else if (age > 14 && age <= 17) {
+      return '고딩';
+    } else if (age > 8 && age <= 14) {
+      return '중딩';
+    } else if (age > 0 && age <= 8) {
+      return '초딩';
+    } else {
+      return '유아';
+    }
   }
 }
 
