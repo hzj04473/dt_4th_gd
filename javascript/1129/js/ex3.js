c31196f85040b5b36958e7bb4afbc46141b5d817
Kim Jinkyoung <hzj04473@naver.com>
2024-11-29 15:21:21 +0900

    1129. javascript ex3 (실습3 - function)

---
+++ javascript/1129/js/ex3.js
@@ -0,0 +1,9 @@
+console.log('function 실습1');
+
+function multifly(num1, num2) {
+  let multi = num1 * num2;
+  return `${num1} * ${num2} = ${multi}`;
+}
+
+console.log(multifly(3, 7));
+console.log(multifly(2, 2));
